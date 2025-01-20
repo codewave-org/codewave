5b3be66cba04c4e7db2af67a63fbf3a2d68b32b4
DeadWaveWave <deadwave233@gmail.com>
2025-01-20 16:55:29 +0800

    fix(build): optimize build configuration - Update ESLint config - Add Next.js config - Update package dependencies

--- frontend/apps/web/.eslintrc.js
+++ frontend/apps/web/.eslintrc.js
@@ -1,11 +1,20 @@
 module.exports = {
+    "root": true,
     "extends": [
-        "next",
+        "next/core-web-vitals",
+        "prettier"
+    ],
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "react-hooks",
         "prettier"
     ],
     "rules": {
         "@typescript-eslint/no-explicit-any": "warn",
         "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
-        "react/react-in-jsx-scope": "off"
-    }
+        "react/react-in-jsx-scope": "off",
+        "prettier/prettier": "error"
+    },
+    "ignorePatterns": ["node_modules/", ".next/", "dist/"]
 } 
